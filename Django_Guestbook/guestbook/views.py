3fdd35d24a9bccd2633c394ceec600c8a073eb5d
asv-trinhtk <admin@duong-s-imac.local>
2014-06-26 12:50:43 +0700

    Khoảng trắng trong code

--- Django_Guestbook/guestbook/views.py
+++ Django_Guestbook/guestbook/views.py
@@ -1,32 +1,24 @@
 # Create your views here.
 import logging
 from django.contrib.databrowse.plugins.calendars import IndexView
-
 from django.http import HttpResponseRedirect
-from django.views.generic.base import TemplateResponseMixin, ContextMixin, View, TemplateView
+from django.views.generic.base import TemplateView
 from google.appengine.api import users
 from guestbook.models import Greeting
-from google.appengine.api import  memcache
+from google.appengine.api import memcache
 import urllib
-
-
-
 
 
 class IndexView(TemplateView):
 
         template_name = "guestbook/mainpage.html"
+
         #Methode get data from database
-
-
         def get_queryset(self, guestbook_name):
-
             return Greeting.query(
                     ancestor=Greeting.get_key_from_name(guestbook_name)).order(-Greeting.date)
 
-
         def get_context_data(self, **kwargs):
-
             guestbook_name = self.request.GET.get('guestbook_name', 'default_guestbook')
             greetings = memcache.get("%s:greetings" %guestbook_name)
             if greetings is None:
@@ -51,15 +43,12 @@ class IndexView(TemplateView):
             context['url_linktext']= url_linktext
             return context
 
-
 class SignView(TemplateView):
 
         template_name = "guestbook/mainpage.html"
 
-
         def post(self, request, *args, **kwargs):
-            
-    #When user signs into guestbook, these following code will help to update greeting's information
+            #When user signs into guestbook, these following code will help to update greeting's information
             guestbook_name = request.POST.get('guestbook_name')
             guestbook_key = Greeting.get_key_from_name(guestbook_name)
             greeting = Greeting(parent=guestbook_key)
