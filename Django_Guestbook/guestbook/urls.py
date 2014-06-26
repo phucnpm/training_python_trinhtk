ceff6a089c25121f502f0c850b1dfeb69c911dca
asv-trinhtk <admin@duong-s-imac.local>
2014-06-26 08:32:33 +0700

    Del Old source

--- Django_Guestbook/guestbook/urls.py
+++ Django_Guestbook/guestbook/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 from guestbook.views import SignView, IndexView
 
 urlpatterns = patterns('',
--- Django_Guestbook/guestbook/views.py
+++ Django_Guestbook/guestbook/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 import logging
-from django.shortcuts import render
+
 from django.http import HttpResponseRedirect
 from django.views.generic.base import TemplateResponseMixin, ContextMixin, View
 from google.appengine.api import users
@@ -8,59 +8,7 @@ from guestbook.models import Greeting
 from google.appengine.api import  memcache
 import urllib
 
-# def main_page(request):
-#      #guestbook_name get from field 'guestbook_name' or default = default_guestbook
-#     guestbook_name = request.GET.get('guestbook_name', 'default_guestbook')
-#
-#     guestbook_key = Greeting.get_key_from_name(guestbook_name)
-#
-#     #Get data from memcache of current guestbook then assign this result for greetings
-#
-#     greetings = memcache.get("%s:greetings" %guestbook_name)
-#     #If memcache does not exist:
-#     if greetings is None:
-#         #Get data from database
-#         #Query get data of greetings whose ancestor is key of current guestbook, sort desc by date
-#         greetings_query = Greeting.query(
-#             ancestor=Greeting.get_key_from_name(guestbook_name)).order(-Greeting.date)
-#         greetings = greetings_query.fetch(10)
-#         #Then cache these data, if app can't cache, give an error message
-#         if not memcache.add("%s:greetings" %guestbook_name, greetings, 10000):
-#             logging.error("Memcache set failed")
-#     #Check whether user loged in
-#     if users.get_current_user():
-#         #Create link logout & text
-#         url = users.create_logout_url(request.get_full_path())
-#         url_linktext = 'Logout'
-#     #Else
-#     else:
-#         #Create link login & text
-#         url = users.create_login_url(request.get_full_path())
-#         url_linktext = 'Login'
-#     #context variables:
-#     template_values = {
-#         'greetings': greetings,
-#         'guestbook_name': guestbook_name,
-#         'url': url,
-#         'url_linktext' : url_linktext,
-#
-#     }
-#      #render template
-#     return render(request, 'guestbook/mainpage.html', template_values)
-# def sign_post(request):
-#     if request.method == 'POST':
-#     #When user signs into guestbook, these following code will help to update greeting's information
-#         guestbook_name = request.POST.get('guestbook_name')
-#         guestbook_key = Greeting.get_key_from_name(guestbook_name)
-#         greeting = Greeting(parent=guestbook_key)
-#         if users.get_current_user():
-#             greeting.author = users.get_current_user().nickname()
-#         greeting.content = request.POST.get('content')
-#     #After put this greeting, clear cache
-#         if greeting.put():
-#             memcache.delete("%s:greetings" %guestbook_name)
-#         return HttpResponseRedirect('/?'+urllib.urlencode({'guestbook_name':guestbook_name}))
-#     return HttpResponseRedirect('/')
+
 
 class TemplateView(TemplateResponseMixin, ContextMixin, View):
     def get(self, request, *args, **kwargs):
