f5f0c05acf33d07d4feb38b29e24812e9d16652e
asv-trinhtk <admin@duong-s-imac.local>
2014-06-26 17:19:07 +0700

    Upload
    project

--- Django_Guestbook/guestbook/models.py
+++ Django_Guestbook/guestbook/models.py
@@ -7,4 +7,8 @@ class Greeting(ndb.Model):
     date = ndb.DateTimeProperty(auto_now_add=True)
     @classmethod
     def get_key_from_name(cls, guestbook_name):
+
         return ndb.Key('Guestbook', guestbook_name or 'default_guestbook')
+    def Update_Info(self, content, author ):
+        self.author = author
+        self.content = content
--- Django_Guestbook/guestbook/views.py
+++ Django_Guestbook/guestbook/views.py
@@ -15,18 +15,19 @@ class IndexView(TemplateView):
         template_name = "guestbook/mainpage.html"
         #Methode get data from database
 
-        def get_queryset(self, guestbook_name):
+        def get_queryset(self, guestbook_key):
 
             return Greeting.query(
-                    ancestor=Greeting.get_key_from_name(guestbook_name)).order(-Greeting.date)
+                    ancestor=guestbook_key).order(-Greeting.date)
 
         def get_context_data(self, **kwargs):
 
             guestbook_name = self.request.GET.get('guestbook_name', 'default_guestbook')
+            guestbook_key = Greeting.get_key_from_name(guestbook_name)
             greetings = memcache.get("%s:greetings" %guestbook_name)
             if greetings is None:
                 #Get data from database
-                greetings = self.get_queryset(guestbook_name).fetch(10)
+                greetings = self.get_queryset(guestbook_key).fetch(10)
                 #Then cache these data, if app can't cache, give an error message
                 if not memcache.add("%s:greetings" %guestbook_name, greetings, 10000):
                     logging.error("Memcache set failed")
@@ -34,11 +35,11 @@ class IndexView(TemplateView):
             #Check whether user loged in
             if users.get_current_user():
                 #Create link logout & text
-                url = users.create_login_url(self.request.get_full_path())
+                url = users.create_logout_url(self.request.get_full_path())
                 url_linktext = 'Logout'
             else:
                 #Create link login & text
-                url = users.create_logout_url(self.request.get_full_path())
+                url = users.create_login_url(self.request.get_full_path())
                 url_linktext = 'Login'
             context['greetings'] = greetings
             context['guestbook_name'] = guestbook_name
@@ -55,11 +56,11 @@ class SignView(TemplateView):
 
             #When user signs into guestbook, these following code will help to update greeting's information
             guestbook_name = request.POST.get('guestbook_name')
-            guestbook_key = Greeting.get_key_from_name(guestbook_name)
-            greeting = Greeting(parent=guestbook_key)
+            greeting = Greeting(parent=Greeting.get_key_from_name(guestbook_name))
             if users.get_current_user():
-                greeting.author = users.get_current_user().nickname()
-            greeting.content = request.POST.get('content')
+                greeting.Update_Info(request.POST.get('content'), users.get_current_user().nickname())
+            else:
+                greeting.Update_Info(request.POST.get('content'), None)
             #After put this greeting, clear cache
             if greeting.put():
                 memcache.delete("%s:greetings" %guestbook_name)
