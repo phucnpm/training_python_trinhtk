7cbb185d4e9898d6e95cdb083f9d232069503be7
asv-trinhtk <admin@duong-s-imac.local>
2014-06-27 10:58:53 +0700

    update

--- Django_Guestbook/guestbook/models.py
+++ Django_Guestbook/guestbook/models.py
@@ -1,6 +1,6 @@
 from google.appengine.ext import ndb
 # Create your models here.
-
+DEFAULT_NAME = 'default_guestbook'
 class Greeting(ndb.Model):
     #variables
     author = ndb.StringProperty()
@@ -9,14 +9,15 @@ class Greeting(ndb.Model):
 
 class Guestbook(ndb.Model):
     name = ndb.StringProperty()
-    default_name = 'default_guestbook'
-    def get_default_name(self):
-        return self.default_name
+
     def get_key(self):
-        return ndb.Key('Guestbook', self.name or self.get_default_name())
+        return ndb.Key(Guestbook, self.name or self.get_default_name())
     def get_latest(self, count):
         return Greeting.query(
                     ancestor= self.get_key()).order(-Greeting.date).fetch(count)
     def put_greeting(self, author, content):
         greeting = Greeting(parent=self.get_key(), author= author, content= content)
         return greeting
+    @classmethod
+    def get_default_name(cls):
+        return DEFAULT_NAME
