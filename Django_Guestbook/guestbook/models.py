e40fb0cfedb89a1fd11c3f2fc49fcfad63689308
asv-trinhtk <admin@duong-s-imac.local>
2014-06-27 10:42:12 +0700

    Fix

--- Django_Guestbook/guestbook/models.py
+++ Django_Guestbook/guestbook/models.py
@@ -1,13 +1,22 @@
 from google.appengine.ext import ndb
 # Create your models here.
+
 class Greeting(ndb.Model):
     #variables
     author = ndb.StringProperty()
     content = ndb.StringProperty()
     date = ndb.DateTimeProperty(auto_now_add=True)
 
-
 class Guestbook(ndb.Model):
     name = ndb.StringProperty()
+    default_name = 'default_guestbook'
+    def get_default_name(self):
+        return self.default_name
     def get_key(self):
-        return ndb.Key('Guestbook', self.name or 'default_guestbook')
+        return ndb.Key('Guestbook', self.name or self.get_default_name())
+    def get_latest(self, count):
+        return Greeting.query(
+                    ancestor= self.get_key()).order(-Greeting.date).fetch(count)
+    def put_greeting(self, author, content):
+        greeting = Greeting(parent=self.get_key(), author= author, content= content)
+        return greeting
--- Django_Guestbook/guestbook/views.py
+++ Django_Guestbook/guestbook/views.py
@@ -1,12 +1,12 @@
 # Create your views here.
 import logging
+import urllib
+from google.appengine.api import users
+from google.appengine.api import memcache
 from django.contrib.databrowse.plugins.calendars import IndexView
 from django.http import HttpResponseRedirect
 from django.views.generic.base import TemplateView
-from google.appengine.api import users
 from guestbook.models import Greeting, Guestbook
-from google.appengine.api import memcache
-import urllib
 
 
 class IndexView(TemplateView):
@@ -15,19 +15,13 @@ class IndexView(TemplateView):
         template_name = "guestbook/mainpage.html"
         #Methode get data from database
 
-        def get_queryset(self, guestbook_key):
-
-            return Greeting.query(
-                    ancestor=guestbook_key).order(-Greeting.date)
-
         def get_context_data(self, **kwargs):
-
             guestbook_name = self.request.GET.get('guestbook_name', 'default_guestbook')
             myGuestbook = Guestbook(name= guestbook_name)
             greetings = memcache.get("%s:greetings" %myGuestbook.name)
             if greetings is None:
                 #Get data from database
-                greetings = self.get_queryset(myGuestbook.get_key()).fetch(10)
+                greetings = myGuestbook.get_latest(10)
                 #Then cache these data, if app can't cache, give an error message
                 if not memcache.add("%s:greetings" %myGuestbook.name, greetings, 10000):
                     logging.error("Memcache set failed")
@@ -53,14 +47,13 @@ class SignView(TemplateView):
         template_name = "guestbook/mainpage.html"
         
         def post(self, request, *args, **kwargs):
-
             #When user signs into guestbook, these following code will help to update greeting's information
             guestbook_name = request.POST.get('guestbook_name')
             myGuestbook = Guestbook(name=guestbook_name)
             if users.get_current_user():
-                greeting = Greeting(parent=myGuestbook.get_key(), author= users.get_current_user().nickname(), content=request.POST.get('content'))
+                greeting = myGuestbook.put_greeting(users.get_current_user().nickname(), request.POST.get('content'))
             else:
-               greeting = Greeting(parent=myGuestbook.get_key(), author= None, content=request.POST.get('content'))
+                greeting = myGuestbook.put_greeting(None, request.POST.get('content'))
             #After put this greeting, clear cache
             if greeting.put():
                 memcache.delete("%s:greetings" %guestbook_name)
