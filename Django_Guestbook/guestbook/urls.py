1fb2a0c3f5be1c6fb95686bbad629b6680af53e7
asv-trinhtk <admin@duong-s-imac.local>
2014-07-24 13:42:00 +0700

    update urls.py

--- Django_Guestbook/guestbook/urls.py
+++ Django_Guestbook/guestbook/urls.py
@@ -5,18 +5,14 @@ from guestbook.views import SignView, IndexView, Send, Delete, Edit
 
 
 urlpatterns = patterns('',
-                       url(r'^sign/$', SignView.as_view(), name='sign'),
-                       url(r'^$', IndexView.as_view(), name ='index'),
-                       url(r'^send/$', Send.as_view(), name ='send'),
-                       url(r'^delete/$', Delete.as_view(), name ='delete'),
-                       url(r'^edit/$', Edit.as_view(), name ='edit'),
-                       url(r'^api/guestbook/(?P<guestbook_name>[a-zA-Z0-9\s\+\_]+)/greeting/$',
-                           Search.as_view()),
-                       url(r'^api/guestbook/'
-                           r'(?P<guestbook_name>[a-zA-Z0-9\s\+\_]+)/greeting/(?P<id>[a-zA-Z0-9\s\+\_]+)/$',
-                           SearchID.as_view()),
-                       url(r'^client/?$',
-                           TemplateView.as_view(template_name='guestbook/client.html')),
-                       url(r'^ui/$',
-                           TemplateView.as_view(template_name='guestbook/index.html'), name='ui')
+    url(r'^sign/$', SignView.as_view(), name='sign'),
+    url(r'^$', IndexView.as_view(), name ='index'),
+    url(r'^send/$', Send.as_view(), name ='send'),
+    url(r'^delete/$', Delete.as_view(), name ='delete'),
+    url(r'^edit/$', Edit.as_view(), name ='edit'),
+    url(r'^api/guestbook/(?P<guestbook_name>[a-zA-Z0-9\s\+\_]+)/greeting/$',Search.as_view()),
+    url(r'^api/guestbook/(?P<guestbook_name>[a-zA-Z0-9\s\+\_]+)/'
+        r'greeting/(?P<id>[a-zA-Z0-9\s\+\_]+)/$',SearchID.as_view()),
+    url(r'^client/?$',TemplateView.as_view(template_name='guestbook/client.html')),
+    url(r'^ui/$',TemplateView.as_view(template_name='guestbook/index.html'), name='ui')
 )
